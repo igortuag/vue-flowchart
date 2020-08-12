878797af77967922994e425664d77e8da3f91c2d
Igor Tuag~ <igortuag@gmail.com>
2020-08-12 17:54:34 -0300

    Add message to card

---
+++ src/views/Board..vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="board">
+    <h1>Board</h1>
+
+    <div v-for="card in cards" :key="card.id">
+      <Card :content="card.content" :cards="card.cards" />
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+import Card from './Card.vue';
+
+export default {
+  data() {
+    return {
+      cards: {},
+    };
+  },
+  components: {
+    Card,
+  },
+  mounted() {
+    axios.get('http://localhost:3000/cards').then((response) => {
+      this.cards = response.data;
+      console.log(response);
+    });
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+*,
+*:before,
+*:after {
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+}
+
+.board {
+  min-width: 1200px;
+  margin: 0;
+  padding: 50px;
+  font: 16px Verdana, sans-serif;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+</style>
---
+++ src/views/Card.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="box">
+    <div class="box__content">
+      <h2 class="card-message__title">Digite a msg</h2>
+      <input class="card-message__input" v-model="inputMessage" type="text" />
+      <span class="card-message__content">
+        {{ content }}
+      </span>
+      <button class="card-message_add" @click="addMessage">+</button>
+    </div>
+    <div class="box__cards">
+      <div v-for="card in cards" :key="card.id">
+        <Card :content="card.content" :cards="card.cards" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  name: 'Card',
+  data() {
+    return {
+      inputMessage: '',
+    };
+  },
+  props: {
+    content: String,
+    cards: Object,
+  },
+  methods: {
+    addMessage() {
+      if (this.cards) {
+        this.cards.push({ content: this.inputMessage });
+      } else {
+        this.cards = [{ content: 'Teste' }];
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+// main-ul => card
+
+.box {
+  border-left: 2px solid black;
+  display: flex;
+  align-items: center;
+  margin-left: 10px;
+
+  &__content {
+    margin-bottom: 15px;
+    background: #c4bdff;
+    width: 100px;
+    height: 100px;
+
+    &::after {
+      content: '';
+      width: 150000px;
+      border-top: 2px solid black;
+    }
+  }
+
+  &:before {
+    content: '';
+    width: 15px;
+    border-top: 2px solid black;
+  }
+
+  &__cards {
+    display: flex;
+    flex-direction: column;
+  }
+}
+
+.card-message__input {
+  width: 70%;
+}
+
+.box__content:before {
+  content: '';
+  width: 15000px;
+  border-top: 2px solid black;
+}
+
+.card-message__title {
+  font-size: 14px;
+}
+</style>
